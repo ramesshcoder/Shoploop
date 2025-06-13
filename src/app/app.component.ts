6cc324978abcabb2ad091fc3a7238dc6161786a1
ramesshcoder <rameshkumarer321@gmail.com>
2025-06-13 15:11:21 +0530

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
--- src/app/seller/seller.component.ts
+++ src/app/seller/seller.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seller',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './seller.component.html',
   styleUrl: './seller.component.css'
 })
 export class SellerComponent {
+  signUp(data:object){
+    console.warn(data)
+
+  }
 
 }
