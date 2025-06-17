e0e95ee0b17a98b26ca4fa9974853403c04593a9
ramesshcoder <rameshkumarer321@gmail.com>
2025-06-17 16:47:04 +0530

    update commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SellerComponent } from './seller/seller.component';
 import { CartComponent } from './cart/cart.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
@@ -13,4 +14,7 @@ export const routes: Routes = [
     {
         path:'cart',component:CartComponent
     },
+    {
+        path:'**',component:PageNotFoundComponent
+    }
 ];
